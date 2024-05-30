adfe6d9279a2705bd5fc816ce1682ba0a4f2552c
Tanmay Shukla <tanmay.shukla629@gmail.com>
2024-05-30 22:26:13 +0530

    created the auto collapse effect for mobile devices

--- app/(browse)/_components/container.tsx
+++ app/(browse)/_components/container.tsx
@@ -2,9 +2,18 @@
 
 import { cn } from '@/lib/utils';
 import { useSidebar } from '@/store/use-sidebar';
+import { useEffect } from 'react';
+import { useMediaQuery } from 'usehooks-ts';
 
 export const Container = ({ children }: { children: React.ReactNode }) => {
+  const matches = useMediaQuery('(max-width: 1024px)');
   const { collapse, onExpand, onCollapse } = useSidebar();
+
+  useEffect(() => {
+    if (matches) {
+      onCollapse();
+    } else onExpand();
+  }, [matches, onCollapse, onExpand]);
   return (
     <div
       className={cn('flex-1', collapse ? 'ml-[70px]' : 'ml-[70px] lg:ml-60')}
