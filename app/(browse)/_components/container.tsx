23a0d0de056395779cf2e9b6212eb6bc6a1d1bbd
Tanmay Shukla <tanmay.shukla629@gmail.com>
2024-05-30 22:14:53 +0530

    added the functionality using the useSidebar module to implement the shifting of content alongwith the expansion and collapse of sidebar

--- app/(browse)/_components/container.tsx
+++ app/(browse)/_components/container.tsx
@@ -1,4 +1,15 @@
 'use client';
+
+import { cn } from '@/lib/utils';
+import { useSidebar } from '@/store/use-sidebar';
+
 export const Container = ({ children }: { children: React.ReactNode }) => {
-  return <div>{children}</div>;
+  const { collapse, onExpand, onCollapse } = useSidebar();
+  return (
+    <div
+      className={cn('flex-1', collapse ? 'ml-[70px]' : 'ml-[70px] lg:ml-60')}
+    >
+      {children}
+    </div>
+  );
 };
